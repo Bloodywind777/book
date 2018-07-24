9ee3faada147c82ea3bec2ad586d4d933818a6df
Bloodywind777 <bloodywind@bk.ru>
2018-07-24 09:36:26 +0300

    Изменили название переменной

--- Boolen.java
+++ Boolen.java
@@ -2,10 +2,10 @@ package book.book;
 
 public class Boolen {
     public static void main(){
-        boolean is;
-        is = false;
+        boolean b;
+        b = false;
 
-        if(is)
+        if(b)
             System.out.println("true");
     }
 }
