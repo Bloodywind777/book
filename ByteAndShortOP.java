20dc1039b73dc84143188efaeef257ccfe2eca5b
Bloodywind777 <bloodywind@bk.ru>
2018-07-25 18:45:48 +0300

    Создание класса ByteAndShortOP Проверка преобразования типов в выражениях byte и short

---
+++ ByteAndShortOP.java
@@ -0,0 +1,17 @@
+package book.book;
+
+public class ByteAndShortOP {
+
+    public static void main(){
+        byte b = 10;
+        short s = 200;
+        int result;
+
+
+        result = (b*s);
+
+        System.out.println(result);
+
+    }
+
+}
