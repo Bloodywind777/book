02deda4a369b008d9137bf759dcbb818a39765b7
Bloodywind777 <bloodywind@bk.ru>
2018-07-25 18:01:18 +0300

    создание метода main

--- LogicalOpTaЬle.java
+++ LogicalOpTaЬle.java
@@ -3,5 +3,31 @@ package book.book;
 public class LogicalOpTaЬle {
     public static void main(){
 
+        boolean p, q;
+
+        System.out.println("P\tQ\tAND\tOR\tNOT");
+        p = q = true;
+        System.out.println(p + "\t"+ q + "\t");
+        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.println((p^q) +"\t"+ (!p));
+
+        p = true;
+        q = false;
+        System.out.println(p +"\t" + q + "\t");
+        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.println((p^q) +"\t"+ (!p));
+
+        p = false;
+        q = true;
+        System.out.println(p +"\t" + q + "\t");
+        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.println((p^q) +"\t"+ (!p));
+
+        p = false;
+        q = false;
+        System.out.println(p +"\t" + q + "\t");
+        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.println((p^q) +"\t"+ (!p));
+
     }
 }
