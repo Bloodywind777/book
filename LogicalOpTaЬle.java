8e7d30f84142bf5f1c52c4914df254e1d5a66aa1
Bloodywind777 <bloodywind@bk.ru>
2018-07-25 18:02:22 +0300

    Исправление вывода таблицы

--- LogicalOpTaЬle.java
+++ LogicalOpTaЬle.java
@@ -7,26 +7,26 @@ public class LogicalOpTaЬle {
 
         System.out.println("P\tQ\tAND\tOR\tNOT");
         p = q = true;
-        System.out.println(p + "\t"+ q + "\t");
-        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.print(p + "\t"+ q + "\t");
+        System.out.print((p&q) +"\t"+ (p|q) +"\t");
         System.out.println((p^q) +"\t"+ (!p));
 
         p = true;
         q = false;
-        System.out.println(p +"\t" + q + "\t");
-        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.print(p +"\t" + q + "\t");
+        System.out.print((p&q) +"\t"+ (p|q) +"\t");
         System.out.println((p^q) +"\t"+ (!p));
 
         p = false;
         q = true;
-        System.out.println(p +"\t" + q + "\t");
-        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.print(p +"\t" + q + "\t");
+        System.out.print((p&q) +"\t"+ (p|q) +"\t");
         System.out.println((p^q) +"\t"+ (!p));
 
         p = false;
         q = false;
-        System.out.println(p +"\t" + q + "\t");
-        System.out.println((p&q) +"\t"+ (p|q) +"\t");
+        System.out.print(p +"\t" + q + "\t");
+        System.out.print((p&q) +"\t"+ (p|q) +"\t");
         System.out.println((p^q) +"\t"+ (!p));
 
     }
